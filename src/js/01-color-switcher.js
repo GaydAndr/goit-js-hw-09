5dea4989f908b14c7602771ed656d196d3ebc00c
andrii_h <galgayand@gmail.com>
2022-06-26 20:34:36 +0300

    dane task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let timerColor;
+
+startBtn.addEventListener('click', changeColor);
+
+stopBtn.addEventListener('click', stopChangingColor);
+
+function changeColor() {
+  timerColor = setInterval(() => {
+    document.body.style.background = getRandomHexColor();
+  }, 1000);
+  startBtn.removeEventListener('click', changeColor);
+}
+
+function stopChangingColor() {
+  clearInterval(timerColor);
+  startBtn.addEventListener('click', changeColor);
+}
