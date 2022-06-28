6bea6db026024ddf7f3db38a0230040bf77c8e92
andrii_h <galgayand@gmail.com>
2022-06-28 18:29:31 +0300

    done task-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
+import Notiflix from 'notiflix';
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', formLsener);
+
+function formLsener(event) {
+  event.preventDefault();
+
+  const { delay, step, amount } = event.currentTarget.elements;
+  // console.log(delay.value, step.value, amount.value);
+
+  for (let i = 0; i < amount.value; i += 1) {
+    let position = i + 1;
+    const delays = Number(delay.value) + step.value * i;
+
+    createPromise(position, delays)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
   }
+  event.currentTarget.reset();
 }
